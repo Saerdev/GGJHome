8922f717260574ac25d46887bab46dc399870131
TaylorColetti <taylorcoletti1@gmail.com>
2019-01-25 23:30:49 -0500

    -Interactables, Inventory

---
+++ Assets/Scripts/IIinteractiable.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IIinteractiable
+{
+    void Interact();
+    
+
+
+    //public bool enter = true;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //     Debug.Log("entered");
+
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    IIinteractiable item;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            item.Interact();
+
+
+
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            item = other.GetComponent<IIinteractiable>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            item = null;
+        }
+    }
+
+}
