ee25befbd1029aadc1abd18481e9264729eee300
Jeffery Erb <ryutwinturbo@yahoo.com>
2019-01-26 13:34:02 -0500

    Added Menu and Credits base

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        //start new game
+    }
+
+    public void Credits()
+    {
+        //run credits
+    }
+
+    public void Exit()
+    {
+        //exit program
+    }
+}
