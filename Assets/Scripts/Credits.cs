94e3b6c66c5e097a93909eac80460ae2cf1eb9f5
Jeffery Erb <ryutwinturbo@yahoo.com>
2019-01-26 19:23:10 -0500

    Added End Credits/ Main Menu

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
+    Animation creditAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        creditAnim = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //scroll credits up screen
+        
     }
 }
